1435c7d43272d8a374c2bd91fd27da48a53c19a9
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-05-21 21:00:36 +0200

    use `meow` in the CLI

--- cli.js
+++ cli.js
@@ -1,28 +1,14 @@
 #!/usr/bin/env node
 'use strict';
-var pkg = require('./package.json');
+var meow = require('meow');
 var randomWord = require('./');
-var argv = process.argv.slice(2);
 
-function help() {
-	console.log([
-		'',
-		'  ' + pkg.description,
-		'',
-		'  Example',
-		'    random-word',
-		'    ferriferous'
-	].join('\n'));
-}
-
-if (argv.indexOf('--help') !== -1) {
-	help();
-	return;
-}
-
-if (argv.indexOf('--version') !== -1) {
-	console.log(pkg.version);
-	return;
-}
+meow({
+	help: [
+		'Example',
+		'  random-word',
+		'  ferriferous'
+	].join('\n')
+});
 
 console.log(randomWord());
